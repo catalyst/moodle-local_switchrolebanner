d9646d0fd5a8a0d210996b9553e7f2e65df0dab6
Rossco Hellmans <rosscohellmans@catalyst-au.net>
2023-09-27 16:49:25 +1000

    Inital commit

---
+++ classes/helper.php
@@ -0,0 +1,144 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Helper class.
+ *
+ * @package    local_switchrolebanner
+ * @author     Rossco Hellmans <rosscohellmans@catalyst-au.net>
+ * @copyright  2023 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace local_switchrolebanner;
+
+use context_course;
+use context_coursecat;
+use html_writer;
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Class helper.
+ *
+ * @package    local_switchrolebanner
+ * @author     Rossco Hellmans <rosscohellmans@catalyst-au.net>
+ * @copyright  2023 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class helper {
+
+    /**
+     * Return if the banner should be shown or not.
+     *
+     * @return bool if the banner should be shown
+     */
+    public static function should_show_banner() : bool {
+        global $PAGE;
+
+        if ($PAGE->course->id == SITEID) {
+            return false;
+        }
+
+        if (!self::has_admin_role()) {
+            return false;
+        }
+
+        if (empty($courseroles = self::get_user_course_role())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if the user has a role to view the current course at a context higher than
+     * the course context.
+     *
+     * @return bool if the user has an admin role
+     */
+    public static function has_admin_role() : bool {
+        global $USER, $COURSE;
+
+        if (isguestuser($USER)) {
+            return false;
+        }
+
+        $context = context_coursecat::instance($COURSE->category, MUST_EXIST);
+
+        return has_capability('moodle/course:view', $context, $USER);
+    }
+
+    /**
+     * Get the user's roles in the course.
+     *
+     * @return array an array of roles
+     */
+    public static function get_user_course_role() : array {
+        global $USER, $COURSE;
+
+        $userid = $USER->id;
+        $context = context_course::instance($COURSE->id, MUST_EXIST);
+        $usersroles = get_users_roles($context, [$userid], false);
+
+        return $usersroles[$userid];
+    }
+
+    /**
+     * Builds and returns the banner HTML.
+     *
+     * @return string banner HTML
+     */
+    public static function get_banner_html() : string {
+        $classes = 'local_switchrolebanner banner alert alert-info';
+        $text  = '<button type="button" class="close" onclick="switchrollebanner_close(this);">×</button>';
+        $text .= '<span><< Switch roll banner placeholder >></span>';
+        $html  = html_writer::div($text, $classes);
+        $html .= self::get_banner_inline_js();
+
+        return $html;
+    }
+
+    /**
+     * Builds and returns the inline banner JS.
+     *
+     * @return string inline banner JS
+     */
+    public static function get_banner_inline_js() : string {
+        $js = <<<EOD
+<script>
+    function switchrollebanner_close(el) {
+        var banner = el.parentElement;
+        var parent = banner.parentElement;
+        var body = document.body;
+        body.classList.remove('local_switchrolebanner_visible');
+        parent.removeChild(banner);
+    }
+    document.addEventListener("DOMContentLoaded", function(event) {
+        document.body.className += ' local_switchrolebanner_visible';
+
+        var banner = document.querySelector('.local_switchrolebanner.banner'),
+            pageHeader = document.querySelector('#page-header'),
+            mainInner = document.querySelector('.main-inner');
+
+        mainInner.insertBefore(banner, pageHeader);
+    });
+</script>
+EOD;
+
+        return $js;
+    }
+}
---
+++ lib.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lib functions.
+ *
+ * @package    local_switchrolebanner
+ * @author     Rossco Hellmans <rosscohellmans@catalyst-au.net>
+ * @copyright  2023 Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use local_switchrolebanner\helper;
+
+/**
+ * before_footer callback to add the banner to the page if conditions are correct.
+ * 
+ * @return string HTML the banner if conditions are correct or an empty string.
+ */
+function local_switchrolebanner_before_footer() {
+    if (helper::should_show_banner()) {
+        return helper::get_banner_html();
+    }
+
+    return '';
+}
